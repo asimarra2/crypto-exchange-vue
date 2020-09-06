5b122c906e5d58e29e68982114c0e7de38b7c8f4
Angelica Simarra <angelicasimarra@gmail.com>
2020-09-06 16:39:57 -0500

    filters, api integration and route were added

---
+++ src/components/PxButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button @click="buttonClick">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'PxButton',
+
+    methods: {
+        buttonClick() {
+            this.$emit('custom-click')
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
     <div>
-        <px-assets-table />
+        <px-assets-table :assetsData="assets" />
     </div>
 </template>
 
 <script>
+import api from '@/api'
 import PxAssetsTable from '@/components/PxAssetsTable.vue'
 export default {
     name: 'Home',
-    components: { PxAssetsTable }
+    components: { PxAssetsTable },
+
+    data() {
+        return {
+            assets: []
+        }
+    },
+
+    created() {
+        api.getAssets().then(assets => (this.assets = assets))
+    }
 }
 </script>
